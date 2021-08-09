e2047d27826c6fb781a329c4055b6d823e1c90d3
mateo-a <alexander.urrego@gmail.com>
2021-08-09 02:35:56 -0500

    [UPDATE] Comment line in file List.js

--- src/components/List.js
+++ src/components/List.js
@@ -5,7 +5,7 @@ import ReactJson from 'react-json-view'
 const List = (props) => {
     const { repos } = props;
     if (!repos || repos.length === 0) return <p>There is not information</p>;
-    console.log("Valor REPO", repos[0]);
+    // console.log("Valor REPO", repos[0]);
     return (
 
         <ReactJson key={repos.id} src={repos} name={"All Countries"} collapsed={1} />
